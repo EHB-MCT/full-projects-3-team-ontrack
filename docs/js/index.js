2f47599fd01d9f7d156b08406b1471ccf60cf2ab
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-01-24 18:41:54 +0100

    Added first two select "pages" to html and js

--- docs/js/index.js
+++ docs/js/index.js
@@ -29,8 +29,162 @@ let index = {
 let trainMaker = {
     checkLanguage() {
         let language = localStorage.getItem("language");
-        document.getElementById(language).style.display = "inherit";
-        
+        let htmlPage = document.getElementById("createTrainPage");
+        let htmlString = ``
+        if (language == "Engels") {
+            htmlString = ` <div id="Engels" class="startpoint">
+            <p>Create your own train</p>
+            <div id="imgPlaceholder">
+                <p>Image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="start">Start</p>
+            </div>
+        </div>`
+        } else if (language == "Nederlands") {
+            htmlString = `  <div id="Nederlands" class="startpoint">
+           <p>Maak jouw eigen trein</p>
+           <div id="imgPlaceholder">
+               <p>afbeelding plaatshouder</p>
+           </div>
+           <div class="buttons" id="startButton">
+               <p id="start">Start</p>
+           </div>
+       </div>`
+        } else if (language == "Frans") {
+            htmlString = `    <div id="Frans" class="startpoint">
+            <p>créez votre propre train</p>
+            <div id="imgPlaceholder">
+                <p>placeur d'image</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="start">Commencer</p>
+            </div>
+        </div>`
+        } else if (language == "Duits") {
+            htmlString = `        <div id="Duits" class="startpoint">
+            <p>Gestalte deinen eigenen Zug</p>
+            <div id="imgPlaceholder">
+                <p>Bild Platzhalter</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="start">Start</p>
+            </div>
+        </div>`
+        }
+
+        htmlPage.innerHTML = htmlString;
+
+        document.getElementById('start').addEventListener('click', (e) => {
+            this.selectFront(htmlPage)
+        })
+
+    },
+    selectFront(htmlPage) {
+        let language = localStorage.getItem("language");
+        htmlPage.innerHTML = ``;
+        document.getElementById('header').style.display = "inherit"
+        let htmlString = ``;
+        if (language == "Engels") {
+            htmlString = ` <div id="Engels" class="options">
+            <p>Choose your locomotive</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectLocomotive">Select</p>
+            </div>
+        </div>`
+        } else if (language == "Nederlands") {
+            htmlString = ` <div id="Nederlands" class="options">
+            <p>Kies jouw locomotief</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectLocomotive">Selecteer</p>
+            </div>
+        </div>`
+        } else if (language == "Frans") {
+            htmlString = ` <div id="Frans" class="options">
+            <p>Choisissez votre locomotive</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectLocomotive">choisissez</p>
+            </div>
+        </div>`
+        } else if (language == "Duits") {
+            htmlString = ` <div id="Duits" class="options">
+            <p>Wählen Sie Ihre Lokomotive</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectLocomotive">auswählen</p>
+            </div>
+        </div>`
+        }
+
+        htmlPage.innerHTML = htmlString
+
+        document.getElementById('selectLocomotive').addEventListener('click', (e) =>{
+            this.selectWagon1(htmlPage)
+        })
+    },
+    selectWagon1(htmlPage){
+        let language = localStorage.getItem("language");
+        htmlPage.innerHTML = ``;
+        document.getElementById('header').innerHTML = ` <img src="../../img/header/header_2.png" alt="header 2" >`
+        let htmlString = ``;
+        if (language == "Engels") {
+            htmlString = ` <div id="Engels" class="options">
+            <p>Choose your first wagon</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectWagon1">Select</p>
+            </div>
+        </div>`
+        } else if (language == "Nederlands") {
+            htmlString = ` <div id="Nederlands" class="options">
+            <p>Kies jouw eerste wagon</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectWagon1">Selecteer</p>
+            </div>
+        </div>`
+        } else if (language == "Frans") {
+            htmlString = ` <div id="Frans" class="options">
+            <p>Choisissez votre premier wagon</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectWagon1">choisissez</p>
+            </div>
+        </div>`
+        } else if (language == "Duits") {
+            htmlString = ` <div id="Duits" class="options">
+            <p>Wählen Sie Ihren ersten Waggon</p>
+            <div id="imgPlaceholderOptions">
+                <p>image placeholder</p>
+            </div>
+            <div class="buttons" id="startButton">
+                <p id="selectWagon1">auswählen</p>
+            </div>
+        </div>`
+        }
+
+        htmlPage.innerHTML = htmlString
+
+        document.getElementById('selectLocomotive').addEventListener('click', (e) =>{
+            this.selectWagon1(htmlPage)
+        })
     }
 }
 
