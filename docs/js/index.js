db2f6bb38b58edc6c40551d1b34a65a39a5a1e0b
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-01-24 15:43:04 +0100

    Created start page

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,28 +1,36 @@
 "use strict";
 
-let language = "";
-
 let index = {
     checkLanguage() {
         document.getElementById('languages').addEventListener('click', (e) => {
-            language = e.target.id;
+            localStorage.setItem('language', e.target.id);
+            let language = localStorage.getItem("language");
             if (language != "languages") {
                 document.getElementById('languages').style.display = "none";
                 document.getElementsByClassName(language)[0].style.display = "inherit";
                 index.chooseOption();
             }
         })
-        
+
     },
     chooseOption() {
+        let language = localStorage.getItem("language");
         document.getElementsByClassName(language)[0].addEventListener('click', (e) => {
             console.log(e.target.id)
             if (e.target.id == "train") {
-                window.location.href = "html/train/locomotive.html";
+                window.location.href = "html/train/startTrain.html";
             } else if (e.target.id == "selfie") {
                 window.location.href = "html/selfie/takeSelfie.html";
             }
         })
+    }
+}
+
+let trainMaker = {
+    checkLanguage() {
+        let language = localStorage.getItem("language");
+        document.getElementById(language).style.display = "inherit";
+        
     }
 }
 
@@ -31,4 +39,9 @@ let index = {
 let indexpage = document.getElementById("indexPage");
 if (indexpage) {
     index.checkLanguage();
+}
+
+let startTrainPage = document.getElementById("createTrainPage");
+if (startTrainPage) {
+    trainMaker.checkLanguage()
 }
